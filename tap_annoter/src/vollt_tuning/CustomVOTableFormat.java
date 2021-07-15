c34f96633a12afe782a64acb6079198ffa86f023
JoannVe <vetterjoann578@gmail.com>
2021-07-15 14:37:38 +0200

    ajout d'un beautifier pour avoir de jolies annotations

--- tap_annoter/src/vollt_tuning/CustomVOTableFormat.java
+++ tap_annoter/src/vollt_tuning/CustomVOTableFormat.java
@@ -10,6 +10,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
 import adql.db.DBColumn;
 
@@ -27,6 +30,7 @@ import utils.FileGetter;
 
 import org.w3c.dom.*;
 import org.w3c.dom.traversal.*;
+import org.xml.sax.InputSource;
 
 public class CustomVOTableFormat extends VOTableFormat {
 
@@ -114,7 +118,8 @@ public class CustomVOTableFormat extends VOTableFormat {
 			  walker.getRoot();
 			  mapper.BuildAnnotations(out,walker,templateDoc);
 			  String finalString = xmlToString(templateDoc); //converting the doc to a string to push it in the buffer
-			  out.write(finalString);
+			  
+			  out.write(toPrettyString(finalString,2));
 
 		    } catch (Exception e) {
 		      e.printStackTrace();
@@ -220,6 +225,47 @@ public class CustomVOTableFormat extends VOTableFormat {
 	        e.printStackTrace();
 	    }
 	    return xmlString;
+	}
+	
+	/**
+	 * @param xml the XML string we want to beautify
+	 * @param indent the number of indentations we want
+	 * @return a beautyfied XML string
+	 */
+	public static String toPrettyString(String xml, int indent) {
+	    try {
+	        // Turn xml string into a document
+	        Document document = DocumentBuilderFactory.newInstance()
+	                .newDocumentBuilder()
+	                .parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
+
+	        // Remove whitespaces outside tags
+	        document.normalize();
+	        XPath xPath = XPathFactory.newInstance().newXPath();
+	        NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+	                                                      document,
+	                                                      XPathConstants.NODESET);
+
+	        for (int i = 0; i < nodeList.getLength(); ++i) {
+	            Node node = nodeList.item(i);
+	            node.getParentNode().removeChild(node);
+	        }
+
+	        // Setup pretty print options
+	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        transformerFactory.setAttribute("indent-number", indent);
+	        Transformer transformer = transformerFactory.newTransformer();
+	        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+	        // Return pretty print xml string
+	        StringWriter stringWriter = new StringWriter();
+	        transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+	        return stringWriter.toString();
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 }
 
